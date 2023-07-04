89ed55c777881b9e4c425a022a6e6e71bd854389
Elis Joynes <smj.joynes@gmail.com>
2023-07-04 17:21:28 -0400

    putting things together

---
+++ Assets/Pen/Pen.cs
@@ -0,0 +1,124 @@
+using System.Collections.Generic;
+using Oculus.Interaction.HandGrab;
+using UnityEngine;
+
+public class Pen : MonoBehaviour, IHandGrabUseDelegate {
+    /// <summary>
+    /// Transform used to target the tip oh the pen.
+    /// </summary>
+    [Tooltip("The tip of the head.")]
+    [SerializeField] private Transform tip;
+    
+    /// <summary>
+    /// Material used to represent the tip.
+    /// </summary>
+    [Tooltip("Material used to represent the tip.")]
+    [SerializeField] private Material tipMaterial;
+    
+    
+    /// <summary>
+    /// Material used to represent the line to draw.
+    /// </summary>
+    [Tooltip("Material used to represent the line to draw.")]
+    [SerializeField] private Material drawingMaterial;
+    
+    /// <summary>
+    /// The with of the line to draw.
+    /// </summary>
+    [Tooltip("The with of the line to draw.")]
+    [SerializeField] [Range(0.01f, .1f)] 
+    private float penWidth = 0.01f;
+    
+    /// <summary>
+    /// The distance that separate 2 key points.
+    /// </summary>
+    [Tooltip("The width of the line to draw.")]
+    [SerializeField] [Range(0.001f, 0.01f)] 
+    private float accuracy = 0.01f; 
+    
+    /// <summary>
+    /// The pen's colour.
+    /// </summary>
+    [Tooltip("The pen's colour.")]
+    [SerializeField] private Color penColour;
+    
+    /// <summary>
+    /// Used to save the lines key points to draw lines. 
+    /// </summary>
+    private Stack<LineRenderer> lineRenderers;
+    
+    /// <summary>
+    /// Counter used to save pen's key points with a unique index.
+    /// </summary>
+    private int index;
+
+    /// <summary>
+    /// Whether or not the user is drawing.
+    /// </summary>
+    private bool isDrawing;
+    
+    
+    private void Start() {
+        tipMaterial.color = penColour;
+        lineRenderers = new Stack<LineRenderer>();
+    }
+
+    private void Update() {
+        if(isDrawing)
+            Draw();
+    }
+
+    /// <summary>
+    /// Clear the current drawing.
+    /// </summary>
+    private void ClearDrawing() {
+        // Destroying all associated game object and removing lines from the stack.
+        while (lineRenderers.Count > 0)
+            Destroy(lineRenderers.Pop().gameObject);
+    }
+
+    private void CreateNewLine() {
+        index = 0;
+        
+        LineRenderer line = new GameObject().AddComponent<LineRenderer>();
+        line.material = drawingMaterial;
+        
+        line.positionCount = 1;
+        line.SetPosition(0, tip.position);
+        
+        line.startWidth = line.endWidth = penWidth;
+        line.startColor = line.endColor = penColour;
+        
+        lineRenderers.Push(line);
+    }
+
+    /// <summary>
+    /// If possible, add a new kay point to the LineRenderer.
+    /// </summary>
+    private void Draw() {
+        LineRenderer currentLine = lineRenderers.Peek(); // Last line used in the drawing
+        
+        // Check if the position is far enough to be considered as a keypoint.
+        Vector3 lastRecordedPosition = currentLine.GetPosition(index);
+        if (Vector3.Distance(lastRecordedPosition, tip.position) < accuracy)
+            return;
+        
+        // Adding a new key point.
+        index++;
+        currentLine.positionCount = index + 1;
+        currentLine.SetPosition(index, tip.position);
+    }
+
+    public void BeginUse() {
+        isDrawing = true;
+        CreateNewLine();
+    }
+
+    public void EndUse() {
+        isDrawing = false;
+    }
+
+    public float ComputeUseStrength(float strength) {
+        return strength;
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using Unity.VisualScripting;
+
+public class InputManager : MonoBehaviour
+{
+    public TMP_InputField pidTextField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartTask()
+    {
+
+        int id = int.Parse(pidTextField.text);
+        PlayerPrefs.SetInt("taskState", -1);
+        PlayerPrefs.SetInt("overallState", 0);
+        PlayerPrefs.SetInt("pid", id);
+
+        // PlayerPrefs.SetInt("ordering", ordering.value);
+
+        //PlayerPrefs.SetInt("left", isLeft.isOn ? 1 : 0);
+        // Debug.Log("Uh hello");
+        string conditionOrderString = GetConditionOrdering(id);
+        // Debug.Log("///// " + conditionOrderString);
+        PlayerPrefs.SetString("conditionOrdering", conditionOrderString);
+
+        SaveData.SetFilePath(id);
+        SceneManager.LoadScene("PenScene");
+    }
+
+    public static string GetConditionOrdering(int pid)
+    {
+
+        int order = pid % 3;
+        // Debug.Log(order);
+        string conditions = "";
+        switch (order)
+        {
+
+            case (0):
+                conditions = "Touch,Index,Pinch";
+                break;
+            case (1):
+                conditions = "Index,Pinch,Touch";
+                break;
+            case (2):
+                conditions = "Pinch,Touch,Index";
+                break;
+
+        }
+
+        return conditions;
+    }
+}
